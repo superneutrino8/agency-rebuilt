236950bda2e538fd2f3dc9b0aa8796b76c4a3488
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-09-15 19:27:09 +0530

    Intro animation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import gsap from "gsap";
 import Banner from "./components/banner";
 import Cases from "./components/cases";
 import Header from "./components/header";
@@ -9,6 +10,36 @@ function App() {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+
+    // timeline - GSAP Animation
+    let tl = gsap.timeline();
+    tl.from(".Banner__line", {
+      y: 100,
+      ease: "power4.out",
+      deplay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.4,
+      },
+    })
+      .to(".Overlay__Top-Single", {
+        height: 0,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .to(".Overlay", { css: { display: "none" } })
+      .to(".Overlay__Bottom-Single", {
+        width: 0,
+        ease: "expo.inOut",
+        delay: -0.8,
+        stagger: {
+          amount: 0.4,
+        },
+      });
   }, []);
 
   return (
